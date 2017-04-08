73113911b0e37d88809f9bd76b54a302b73f9294
Hunan Rostomyan <hunan131@gmail.com>
2017-04-07 21:07:33 -0700

    Add tests for disjoint set implementation; fix spacing issues

---
+++ ds/disjointset/disjointset_test.py
@@ -0,0 +1,27 @@
+from disjointset import DisjointSet
+
+
+def test_count():
+    ds = DisjointSet(5)
+    assert ds.count() == 5
+
+
+def test_find():
+    ds = DisjointSet(5)
+    assert ds.find(2) == 2
+    assert ds.find(3) == 3
+
+
+def test_connected():
+    ds = DisjointSet(5)
+    assert not ds.connected(1, 2)
+    ds.union(1, 2)
+    assert ds.connected(1, 2)
+
+
+def test_union():
+    ds = DisjointSet(5)
+    assert not ds.connected(2, 3)
+    ds.union(2, 3)
+    assert ds.connected(2, 3)
+
--- ds/linkedlist/doubly_test.py
+++ ds/linkedlist/doubly_test.py
@@ -4,34 +4,34 @@ from util.link import links_from_list
 
 
 def test_iter():
-	linker = partial(links_from_list, Link)
-	ll = linker([2, 4])
-	assert set(ll) == {2, 4}
+    linker = partial(links_from_list, Link)
+    ll = linker([2, 4])
+    assert set(ll) == {2, 4}
 
 
 def test_insert():
-	ll = Link()
-	assert not ll.search(5)
+    ll = Link()
+    assert not ll.search(5)
 
-	ll.insert(Link(5))	
-	assert ll.search(5).datum == 5
+    ll.insert(Link(5))  
+    assert ll.search(5).datum == 5
 
 
 def test_search():
-	linker = partial(links_from_list, Link)
-	ll = linker([2, 4])
+    linker = partial(links_from_list, Link)
+    ll = linker([2, 4])
 
-	assert not ll.search(1)
-	assert not ll.search(3)
-	assert ll.search(2)
-	assert ll.search(4)
+    assert not ll.search(1)
+    assert not ll.search(3)
+    assert ll.search(2)
+    assert ll.search(4)
 
 
 def test_delete():
-	linker = partial(links_from_list, Link)
-	ll = linker([2])
+    linker = partial(links_from_list, Link)
+    ll = linker([2])
 
-	assert ll.search(2)
+    assert ll.search(2)
 
-	ll.delete(Link(2))
-	assert not ll.search(2)
+    ll.delete(Link(2))
+    assert not ll.search(2)
